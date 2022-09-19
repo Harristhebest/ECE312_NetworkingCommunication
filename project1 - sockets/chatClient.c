caa97d29803517c042740b0ae197d2abd78d7d50
harristhebest <harriswuqi@gmail.com>
2022-09-18 22:32:46 -0400

    finished

--- project1 - sockets/chatClient.c
+++ project1 - sockets/chatClient.c
@@ -1,12 +1,31 @@
-   // From page 58, Figure 2.14 of Stallings
-   
+   /* project 1 sockets programming
+   File name: chatClient.c
+   Author: Harris Wu
+   Date: 9/18/2022
+   Description: 
+                This program sets a communication channel between two devices, this file is the client side.
+
+    Function Description: 
+                As the client, once the server starts the listening process, we will need to provide the username
+                 as well as the ip address of the server in order to set up the channel.
+
+    Note: Typing "quit" will terminate the program
+
+    Important Variables:
+        sockfd: integer that identifies the socket
+        portno: port number 
+        n: error indicator
+        serv_addr: address of the server
+        buffer: buffer for the communication info
+        username: username of the other end
+        localname: username of the local end
+   */
    #include <stdio.h>
    #include <sys/types.h>
    #include <sys/socket.h>
    #include <netinet/in.h>
    #include <netdb.h> 
-     #include <signal.h>
-
+   #include <signal.h>
    #include <stdlib.h>
    #include <string.h>
    #include <unistd.h>
@@ -21,58 +40,58 @@
 
    int main(int argc, char *argv[])
    {
+    //variables declearation
      int sockfd, portno, n;
-
      struct sockaddr_in serv_addr;
      struct hostent *server;
      char buffer[256];
      char username[256];
     char local_name[256]="";
-     if (argc < 3) {
+     if (argc < 3) {                            //if the argument is not enough
         fprintf(stderr,"usage %s hostname port\n", argv[0]);
         exit(0);
      }
-     portno = atoi(argv[2]);
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+
+     portno = atoi(argv[2]);                    //port number assignment
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);  //get the socket identification 
+
+     if (sockfd < 0)                            //if socket fails
          error("ERROR opening socket");
-     server = gethostbyname(argv[1]);
+
+     server = gethostbyname(argv[1]);           //get the host 
      if (server == NULL) {
          fprintf(stderr,"ERROR, no such host\n");
          exit(0);
      }
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     serv_addr.sin_family = AF_INET;
-     bcopy((char *)server->h_addr, 
+
+     bzero((char *) &serv_addr, sizeof(serv_addr)); //clean the memory space for the variables 
+     serv_addr.sin_family = AF_INET;                //assign the struct variables AF_INET specifies the domain
+     bcopy((char *)server->h_addr,                  //address assignment
           (char *)&serv_addr.sin_addr.s_addr,
           server->h_length);
-     serv_addr.sin_port = htons(portno);
+     serv_addr.sin_port = htons(portno);            //port number
      if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
          error("ERROR connecting");
-     printf("please enter your username:");
-     bzero(local_name,256);
+
+     printf("please enter your username:");         //ask for the client to provide the username
+     bzero(local_name,256);                         
      fgets(local_name,256,stdin);
     local_name[strcspn(local_name, "\n")] = '\0';
 
-
-
     printf("ip address: %s\n", argv[1]);
-
-
-
-
-
-    bzero(username,255);
+    printf("waiting for connection...\n");
+    bzero(username,255);                            //get the username of the other end
     n = read(sockfd,username,255);
     if (n < 0) error("ERROR reading from socket");
-    printf("server username recieved\n");     
+    printf("server username recieved\n");    
 
-    n = write(sockfd,argv[1],strlen(argv[1]));  //ip addr
-    if (n < 0) 
-        error("ERROR writing to socket");
-    printf("client ip transmitted\n");
 
-    n = write(sockfd,local_name,strlen(local_name));        //username
+    bzero(buffer,256);
+    strcpy(buffer,argv[1]);
+    char info [] = ",";
+    strcat(buffer,info);
+    strcat(buffer,local_name);
+    n = write(sockfd,buffer,strlen(buffer));        //send the username to the other end
     if (n < 0) 
         error("ERROR writing to socket");
     printf("client username transmitted\n");
@@ -80,24 +99,33 @@
 
 
     bzero (buffer,256);
+    /*
+    multi-process begines here
+    Child: listening to the server
+    Parent: writing to the server
+    The reason why I use multi threads here is to ensure listenning and reading runs at the same time
+    */
     pid_t pid = fork();
-    //writing
+
+    //writing @parent 
     if(pid!=0){
         printf("now writting starts\n");
+        /*
+        this while loop will end once either side typed "quit" into the propt, same for the 
+        while loop in the child process
+        */
         while (strcmp(buffer,"quit\n")!=0){
-            printf("%s->",local_name);
-            bzero(buffer,256);
-            fgets(buffer,256,stdin);
-            buffer[strcspn(buffer, "\n")] = '\0';
-            n = write(sockfd,buffer,strlen(buffer));            
+            printf("%s->",local_name);  
+            bzero(buffer,256);          //clean the buffer space
+            fgets(buffer,256,stdin);    //get the info from the prompt
+            n = write(sockfd,buffer,strlen(buffer));  //send info          
             if (n < 0) 
                 error("ERROR writing to socket");   
         }
     }
-
+    //reading @child
     else{    
-        
-        while (strcmp(buffer,"quit\n")!=0){
+        while (strcmp(buffer,"quit\n")!=0){ 
             bzero(buffer,255);
             n = read(sockfd,buffer,255);
             if (n < 0) 
@@ -105,8 +133,8 @@
             printf("\n %s-> %s",username,buffer);
         }
     }
-    kill(pid,SIGKILL);
-    close(sockfd);
+    kill(pid,SIGKILL);  //kill the process
+    close(sockfd);      //close the socket
     printf("Client Program ended\n");
      return 0;
    }
--- project1 - sockets/chatServer.c
+++ project1 - sockets/chatServer.c
@@ -1,5 +1,30 @@
-// From page 57, Figure 2.13 of Stallings
+   /* project 1 sockets programming
+   File name: chatServer.c
+   Author: Harris Wu
+   Date: 9/18/2022
+   File Description: this program sets a communication channel between two devices, this file is the Server side.
 
+   Function description:
+                As the Server, the listenning process will begin once the username has been typed into the prompt
+                once the connection request is recieved.
+    Note: Typing "quit" will terminate the program
+
+    Important Variables:
+        sockfd: integer that identifies the socket
+        portno: port number
+        n: error indicator
+        serv_addr: address of the server
+        cli_addr: address of the client
+        buffer: buffer for the communication info
+        username: username of the other end
+        localname: username of the local end
+
+        struct sockaddr_in:
+        //this struct basically includes all the information of the socket address. Here's its structure:
+        //sin_family: address family TCP/IP
+        //sin_port: port number
+        //in_addr sin_addr: internet address
+   */
   #include <stdio.h>
   #include <sys/types.h> 
   #include <sys/socket.h>
@@ -19,78 +44,74 @@
    int main(int argc, char *argv[])
    {    
 
-        int sockfd, newsockfd, portno, clilen;
+        int sockfd, newsockfd, portno, clilen;  //see the description of the struct sockaddr_in in the header
         char buffer[256];
-        char username[256]; //other end 
-        char local_name[256];
+        char* username = malloc(256);     //other end 
+        char local_name[256];   //local end
         pid_t pid = -1;
         struct sockaddr_in serv_addr, cli_addr;
+
         int n;
         if (argc < 2) {
             fprintf(stderr,"ERROR, no port provided\n");
             exit(1);
         }
+        //initiate program
         sockfd = socket(AF_INET, SOCK_STREAM, 0);
         if (sockfd < 0) 
            error("ERROR opening socket");
         bzero((char *) &serv_addr, sizeof(serv_addr));
-        portno = atoi(argv[1]);
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(portno);
-        serv_addr.sin_addr.s_addr = INADDR_ANY;
+        portno = atoi(argv[1]);                             //convert port number to int
+        serv_addr.sin_family = AF_INET;                     //domain
+        serv_addr.sin_port = htons(portno);                 //port
+        serv_addr.sin_addr.s_addr = INADDR_ANY;             //address
+
         if (bind(sockfd, (struct sockaddr *) &serv_addr,
                  sizeof(serv_addr)) < 0) 
                  error("ERROR on binding");
-        listen(sockfd,5);
+        
+        listen(sockfd,5);                                   //listening initiated
         clilen = sizeof(cli_addr);
+
         printf("waiting for connection...\n");
+        //finding the request, accept the request once found
         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
         if (newsockfd < 0) 
              error("ERROR on accept");
 
-
+        //get the username
         printf("please enter your user name:");
         bzero(local_name,strlen(local_name));
         fgets(local_name,256,stdin);
         local_name[strcspn(local_name,"\n")] = '\0';
 
-
+        //give client the username
         n = write(newsockfd,local_name,255);
         if (n < 0) error("ERROR writing to socket");
         printf("usernamer transmmited to the client\n");
 
         printf("now waiting for the other end to provide the ip addr...\n");    
-
-
-
-    
-
-
-
-
-
-        bzero(username,256);
+        //wait for the client to provide the username and ip address
+        bzero(buffer,256);
         n = read(newsockfd,buffer,255);
         if (n < 0) error("ERROR reading from socket");
-        printf("username recieved from client\n");
+        char* ip_addr = strtok(buffer,",");    
+        memset(username,0,256);    
+        username = strtok(NULL,",");
+
+        printf("Connection established with: %s , username : %s\n",ip_addr,username);
 
 
-
-        bzero(buffer,256);
-        n = read(newsockfd,username,255);
-        if (n < 0) error("ERROR reading from socket");
-        printf("id addr recieved from client\n");
-
-        printf("Connection established with: %s , username : %s\n",buffer,username);
-
-        pid = fork();
-        // printf("multi thread creating... pid = %d\n",pid);
         
-
-
-        //write messages 
+        //multi-thread begins here to allow reading/writing running at the same time
+        pid = fork();
+        
+        //write messages @child
         if(pid==0){
-            // printf("writting successfully initiated\n");
+            /*
+            this while loop will end once either side typed "quit" into the propt, same for the 
+            while loop in the child process
+            */
             while (strcmp(buffer,"quit\n")!=0){
                 printf("\n%s->",local_name);
                 bzero(buffer,256);
@@ -101,21 +122,18 @@
                     error("ERROR writing to socket");  
             }
         } 
-        //read messages
+        //read messages @parent
         else{
-            // printf("reading successfully initiated\n");
             while (strcmp(buffer,"quit\n")!=0){
-                printf("\n%s->",local_name);
-
                 bzero(buffer,256);
                 n = read(newsockfd,buffer,255);
                 if (n < 0) error("ERROR reading from socket");
                 printf("\n%s -> %s",username, buffer);
             }
         }
-        kill(pid, SIGKILL);
-        printf("program terminated");
-        close(newsockfd);
+        kill(pid, SIGKILL); //kill the process
+        printf("program terminated\n");
+        close(newsockfd);   //close the socket channel
         return 0; 
    }
 
